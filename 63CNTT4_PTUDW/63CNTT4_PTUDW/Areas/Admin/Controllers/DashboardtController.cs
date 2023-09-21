7aefcdd7685e8baaefcd7b4ffd161dfda8854b69
HuynhGiaBao1409 <145564674+HuynhGiaBao1409@users.noreply.github.com>
2023-09-21 20:27:04 +0700

    Begin Project

---
+++ 63CNTT4_PTUDW/63CNTT4_PTUDW/Areas/Admin/Controllers/DashboardtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _63CNTT4_PTUDW.Areas.Admin.Controllers
+{
+    public class DashboardtController : Controller
+    {
+        // GET: Admin/Dashboardt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 63CNTT4_PTUDW/63CNTT4_PTUDW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_63CNTT4_PTUDW.MvcApplication" Language="C#" %>
