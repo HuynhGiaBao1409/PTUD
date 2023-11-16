e34f221af470835183cdc4d6c228f4ffca187d6e
HuynhGiaBao1409 <bao.hg.63cntt@ntu.edu.vn>
2023-11-16 18:49:23 +0700

    Categories

---
+++ OnlineShoppingStore/OnlineShoppingStore/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public GenericUnitOfWork _unitOfWork= new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories= _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i=>i.IsDelete==false).ToList();
+            return View(allcategories);
+        }
+    }
+}
